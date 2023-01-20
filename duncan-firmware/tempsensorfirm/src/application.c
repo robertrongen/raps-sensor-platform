c5c74ed7c010037d3bf4158713480b2469d6a2d0
robertrongen <robert.rongen@blockchainadvies.nu>
2023-01-20 14:55:29 +0100

    Updated code to match Smartys sensor code

--- duncan-firmware/tempsensorfirm/src/application.c
+++ duncan-firmware/tempsensorfirm/src/application.c
@@ -4,27 +4,6 @@
 
 #include <application.h>
 
-// #define SERVICE_INTERVAL_INTERVAL (60 * 60 * 1000)
-// #define BATTERY_UPDATE_INTERVAL   (60 * 60 * 1000)
-// #define BATTERY_UPDATE_INTERVAL   (60 * 60 * 1000)
-
-// #define UPDATE_SERVICE_INTERVAL            (5 * 1000)
-// #define UPDATE_NORMAL_INTERVAL             (10 * 1000)
-// #define BAROMETER_UPDATE_SERVICE_INTERVAL  (1 * 60 * 1000)
-// #define BAROMETER_UPDATE_NORMAL_INTERVAL   (5 * 60 * 1000)
-
-// #define TEMPERATURE_TAG_PUB_NO_CHANGE_INTEVAL (15 * 60 * 1000)
-// #define TEMPERATURE_TAG_PUB_VALUE_CHANGE 0.2f
-
-// #define HUMIDITY_TAG_PUB_NO_CHANGE_INTEVAL (15 * 60 * 1000)
-// #define HUMIDITY_TAG_PUB_VALUE_CHANGE 5.0f
-
-// #define LUX_METER_TAG_PUB_NO_CHANGE_INTEVAL (15 * 60 * 1000)
-// #define LUX_METER_TAG_PUB_VALUE_CHANGE 25.0f
-
-// #define BAROMETER_TAG_PUB_NO_CHANGE_INTEVAL (15 * 60 * 1000)
-// #define BAROMETER_TAG_PUB_VALUE_CHANGE 20.0f
-
 all_settings_t settings;
 
 struct
@@ -38,12 +17,8 @@ struct
 // LED instance
 twr_led_t led;
 
-// Button instance
-twr_button_t button;
-
 // Thermometer instance
 twr_tmp112_t tmp112;
-uint16_t button_click_count = 0;
 
 uint64_t _radio_id;
 
@@ -57,7 +32,7 @@ void twr_get_config(uint64_t *id, const char *topic, void *value, void *param);
 
 static const twr_radio_sub_t subs[] = {
     {"denurity/-/get/config", TWR_RADIO_SUB_PT_STRING, twr_get_config, NULL}
-    //{"denurity/-/get/config", TWR_RADIO_SUB_PT_STRING, twr_get_config, NULL}
+    // {"denurity/-/get/config", TWR_RADIO_SUB_PT_STRING, twr_get_config, NULL}
     };
 
 void twr_get_config(uint64_t *id, const char *topic, void *value, void *param)
@@ -82,19 +57,34 @@ void twr_get_config(uint64_t *id, const char *topic, void *value, void *param)
     settings.UPDATE_NORMAL_INTERVAL = array[3];
     settings.BAROMETER_UPDATE_SERVICE_INTERVAL = array[4];
     settings.BAROMETER_UPDATE_NORMAL_INTERVAL = array[5];
-    settings.TEMPERATURE_TAG_PUB_NO_CHANGE_INTEVAL = array[6];
+    settings.TEMPERATURE_TAG_PUB_NO_CHANGE_INTERVAL = array[6]; //
     settings.TEMPERATURE_TAG_PUB_VALUE_CHANGE = array[7];
-    settings.HUMIDITY_TAG_PUB_NO_CHANGE_INTEVAL = array[8];
+    settings.HUMIDITY_TAG_PUB_NO_CHANGE_INTERVAL = array[8]; //
     settings.HUMIDITY_TAG_PUB_VALUE_CHANGE = array[9];
-    settings.LUX_METER_TAG_PUB_NO_CHANGE_INTEVAL = array[10];
+    settings.LUX_METER_TAG_PUB_NO_CHANGE_INTERVAL = array[10]; //
     settings.LUX_METER_TAG_PUB_VALUE_CHANGE = array[11];
-    settings.BAROMETER_TAG_PUB_NO_CHANGE_INTEVAL = array[12];
+    settings.BAROMETER_TAG_PUB_NO_CHANGE_INTERVAL = array[12]; //
     settings.BAROMETER_TAG_PUB_VALUE_CHANGE = array[13];
 
     update_done = true;
     new_update = false;
 
     twr_log_info("config loaded. executing main methods");
+}
+
+void battery_event_handler(twr_module_battery_event_t event, void *event_param)
+{
+    (void) event_param;
+
+    float voltage;
+
+    if (event == TWR_MODULE_BATTERY_EVENT_UPDATE)
+    {
+        if (twr_module_battery_get_voltage(&voltage))
+        {
+            twr_radio_pub_battery(&voltage);
+        }
+    }
 }
 
 void climate_module_event_handler(twr_module_climate_event_t event, void *event_param)
@@ -111,7 +101,7 @@ void climate_module_event_handler(twr_module_climate_event_t event, void *event_
             {
                 twr_radio_pub_temperature(TWR_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_DEFAULT, &value);
                 params.temperature.value = value;
-                params.temperature.next_pub = twr_scheduler_get_spin_tick() + settings.TEMPERATURE_TAG_PUB_NO_CHANGE_INTEVAL;
+                params.temperature.next_pub = twr_scheduler_get_spin_tick() + settings.TEMPERATURE_TAG_PUB_NO_CHANGE_INTERVAL;
             }
         }
     }
@@ -123,7 +113,7 @@ void climate_module_event_handler(twr_module_climate_event_t event, void *event_
             {
                 twr_radio_pub_humidity(TWR_RADIO_PUB_CHANNEL_R3_I2C0_ADDRESS_DEFAULT, &value);
                 params.humidity.value = value;
-                params.humidity.next_pub = twr_scheduler_get_spin_tick() + settings.HUMIDITY_TAG_PUB_NO_CHANGE_INTEVAL;
+                params.humidity.next_pub = twr_scheduler_get_spin_tick() + settings.HUMIDITY_TAG_PUB_NO_CHANGE_INTERVAL;
             }
         }
     }
@@ -140,7 +130,7 @@ void climate_module_event_handler(twr_module_climate_event_t event, void *event_
             {
                 twr_radio_pub_luminosity(TWR_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_DEFAULT, &value);
                 params.illuminance.value = value;
-                params.illuminance.next_pub = twr_scheduler_get_spin_tick() + settings.LUX_METER_TAG_PUB_NO_CHANGE_INTEVAL;
+                params.illuminance.next_pub = twr_scheduler_get_spin_tick() + settings.LUX_METER_TAG_PUB_NO_CHANGE_INTERVAL;
             }
         }
     }
@@ -159,10 +149,20 @@ void climate_module_event_handler(twr_module_climate_event_t event, void *event_
 
                 twr_radio_pub_barometer(TWR_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_DEFAULT, &value, &meter);
                 params.pressure.value = value;
-                params.pressure.next_pub = twr_scheduler_get_spin_tick() + settings.BAROMETER_TAG_PUB_NO_CHANGE_INTEVAL;
+                params.pressure.next_pub = twr_scheduler_get_spin_tick() + settings.BAROMETER_TAG_PUB_NO_CHANGE_INTERVAL;
             }
         }
     }
+}
+
+void switch_to_normal_mode_task(void *param)
+{
+    twr_module_climate_set_update_interval_thermometer(settings.UPDATE_NORMAL_INTERVAL);
+    twr_module_climate_set_update_interval_hygrometer(settings.UPDATE_NORMAL_INTERVAL);
+    twr_module_climate_set_update_interval_lux_meter(settings.UPDATE_NORMAL_INTERVAL);
+    twr_module_climate_set_update_interval_barometer(settings.BAROMETER_UPDATE_NORMAL_INTERVAL);
+
+    twr_scheduler_unregister(twr_scheduler_get_current_task_id());
 }
 
 // Application initialization function which is called once after boot
@@ -171,9 +171,25 @@ void application_init(void)
     // Initialize logging
     twr_log_init(TWR_LOG_LEVEL_DUMP, TWR_LOG_TIMESTAMP_ABS);
 
-    settings.BAROMETER_UPDATE_SERVICE_INTERVAL = (60 * 60 * 1000);
-    settings.TEMPERATURE_TAG_PUB_NO_CHANGE_INTEVAL = 15 * 60 * 1000;
-    settings.TEMPERATURE_TAG_PUB_VALUE_CHANGE = 0.2f;
+    // 
+    settings.SERVICE_INTERVAL_INTERVAL              = (60 * 60 * 1000);
+    settings.BATTERY_UPDATE_INTERVAL                = (60 * 60 * 1000);
+    settings.UPDATE_SERVICE_INTERVAL                = (5 * 1000);
+    settings.UPDATE_NORMAL_INTERVAL                 = (10 * 1000);
+    settings.BAROMETER_UPDATE_SERVICE_INTERVAL      = (1 * 60 * 1000);
+    settings.BAROMETER_UPDATE_NORMAL_INTERVAL       = (5 * 60 * 1000);
+    settings.TEMPERATURE_TAG_PUB_NO_CHANGE_INTERVAL = 15 * 60 * 1000;
+    settings.TEMPERATURE_TAG_PUB_VALUE_CHANGE       = 0.2f;
+    settings.HUMIDITY_TAG_PUB_NO_CHANGE_INTERVAL    = (15 * 60 * 1000);
+    settings.HUMIDITY_TAG_PUB_VALUE_CHANGE          = 5.0f;
+    settings.LUX_METER_TAG_PUB_NO_CHANGE_INTERVAL   = (15 * 60 * 1000);
+    settings.LUX_METER_TAG_PUB_VALUE_CHANGE         = 25.0f;
+    settings.BAROMETER_TAG_PUB_NO_CHANGE_INTERVAL   = (15 * 60 * 1000);
+    settings.BAROMETER_TAG_PUB_VALUE_CHANGE         = 20.0f;
+
+    // Initialize LED
+    twr_led_init(&led, TWR_GPIO_LED, false, false);
+    twr_led_set_mode(&led, TWR_LED_MODE_OFF);
 
     // Initialize thermometer on core module
     twr_tmp112_init(&tmp112, TWR_I2C_I2C0, 0x49);
@@ -183,8 +199,27 @@ void application_init(void)
     twr_radio_set_rx_timeout_for_sleeping_node(500);
     twr_radio_set_subs((twr_radio_sub_t *)subs, sizeof(subs) / sizeof(twr_radio_sub_t));
 
+    // Initialize battery
+    twr_module_battery_init();
+    twr_module_battery_set_event_handler(battery_event_handler, NULL);
+    twr_module_battery_set_update_interval(settings.BATTERY_UPDATE_INTERVAL);
+
+    // Initialize climate module
+    twr_module_climate_init();
+    twr_module_climate_set_event_handler(climate_module_event_handler, NULL);
+    twr_module_climate_set_update_interval_thermometer(settings.UPDATE_SERVICE_INTERVAL);
+    twr_module_climate_set_update_interval_hygrometer(settings.UPDATE_SERVICE_INTERVAL);
+    twr_module_climate_set_update_interval_lux_meter(settings.UPDATE_SERVICE_INTERVAL);
+    twr_module_climate_set_update_interval_barometer(settings.BAROMETER_UPDATE_SERVICE_INTERVAL);
+    twr_module_climate_measure_all_sensors();
+
     // Send radio pairing request
     twr_radio_pub_bool("sensor/start/get/config", true);
+
+    twr_scheduler_register(switch_to_normal_mode_task, NULL, settings.SERVICE_INTERVAL_INTERVAL);
+
+    twr_led_pulse(&led, 2000);
+
 }
 
 // Application task function (optional) which is called peridically if scheduled
@@ -200,7 +235,7 @@ void application_task(void)
             char *id = malloc(32);
             sprintf(id, "%llX", _radio_id);
             twr_log_debug("register with ID %s", id);
-            twr_radio_pairing_request(id, 1);
+            twr_radio_pairing_request(id, "1");
         }
         twr_scheduler_plan_current_from_now(updateSchedule);
         return;
@@ -218,7 +253,7 @@ void application_task(void)
             twr_module_climate_measure_all_sensors();
 
             twr_module_climate_set_event_handler(climate_module_event_handler, NULL);
-            twr_tmp112_set_event_handler(&tmp112, TWR_I2C_I2C0, 0x49);
+            twr_tmp112_set_event_handler(&tmp112, TWR_I2C_I2C0, "0x49");
             first_update_done = true;
         }
         else
